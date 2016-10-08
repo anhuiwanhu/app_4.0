d928d3a0f25d72dcb8585032812cc3e41eb0d3c8
lvguodong <1071858797@qq.com>
2016-10-08 17:18:52 +0800

    evo4.0_SP_04_20160620

--- ms_sql.sql
+++ ms_sql.sql
@@ -14,3 +14,10 @@ go
 
 insert into oa_patchinfo (patch_editinfo,patch_name,patch_version,patch_time,PATCH_EVO) values('evo','evo4.0_SP_03_20160605','4.0.03',getdate(),1);
 go
+
+
+
+
+
+insert into oa_patchinfo (patch_editinfo,patch_name,patch_version,patch_time,PATCH_EVO) values('evo','evo4.0_SP_04_20160620','4.0.04',getdate(),1);
+go
--- oracle.sql
+++ oracle.sql
@@ -15,3 +15,9 @@ commit;
 insert into oa_patchinfo (patch_id,patch_editinfo,patch_name,patch_version,patch_time,PATCH_EVO) values(hibernate_sequence.nextval,'evo','evo4.0_SP_03_20160605','4.0.03',sysdate,1);
 commit;
 
+
+
+
+
+insert into oa_patchinfo (patch_id,patch_editinfo,patch_name,patch_version,patch_time,PATCH_EVO) values(hibernate_sequence.nextval,'evo','evo4.0_SP_04_20160620','4.0.04',sysdate,1);
+commit;
